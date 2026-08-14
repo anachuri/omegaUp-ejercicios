bd45f75aa4bd4ab9f5770ec2ed0e3baeae55c02b
= <maxigroxx@gmail.com>
2026-08-14 13:25:14 -0300

    agrego metodo para consulta por rangos

--- alquimia_felina.c
+++ alquimia_felina.c
@@ -15,19 +15,33 @@ void create_tree(Node *node,char arr[],int n){
   if(n%2==1 && node[n].c == node[n+1].c)
     node[n].groups=1;
   for (int i = n - 1; i > 0; i--) {      
-        if(node[i << 1].c == 'z' && node[i << 1 | 1].c == 'z')
+        if(node[i << 1].c == 'z' && node[i << 1 | 1].c == 'z' || node[i << 1].c != node[i << 1 | 1].c)
           node[i].groups = node[i << 1].groups + node[i << 1 | 1].groups;
-        else if(node[i << 1].c == node[i << 1 | 1].c )
-            node[i].groups=1;
         else
-            node[i].groups = node[i << 1].groups + node[i << 1 | 1].groups;  
+            node[i].groups=1;
   }
     
-  for(int i=0;i<n*2;i++){
+ /* for(int i=0;i<n*2;i++){
     printf("groups en i=%d, v:%d ,c %c\n",i,node[i].groups,node[i].c);
-  }
-  
+  }*/
   printf("groups::: %d\n",node[1].groups);
+}
+
+
+int query(Node* node, int l, int r,int n) {
+    int res = 0;
+    // 1. Mover los índices del arreglo original a las HOJAS del Segment Tree
+    l += n;
+    r += n + 1; // Convertir a rango semiabierto [l, r)
+    // 2. Recorrer de abajo hacia arriba en el árbol
+    while (l < r) {
+        if (l & 1) res += node[l++].groups; // Si 'l' es hijo derecho
+        if (r & 1) res += node[--r].groups; // Si 'r' es hijo derecho
+        
+        l >>= 1; // Subir 'l' a su padre (l = l / 2)
+        r >>= 1; // Subir 'r' a su padre (r = r / 2)
+    }
+    return res;
 }
 
 int main(int argc, char *argv[]) {
@@ -41,5 +55,9 @@ int main(int argc, char *argv[]) {
   //printf("%d\n", consultar(st, 1, 5));  
   Node tree[n*2];
   create_tree(tree,cadena,n);
+  
+  for(int i=0;i<q;i++){
+    scanf("%d", &);
+  }
   return 0;
 }
