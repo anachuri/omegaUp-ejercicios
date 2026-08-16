db9a44af354eb42b8df4863b725a2ea68fcaf075
= <maxigroxx@gmail.com>
2026-08-16 03:50:01 -0300

    agrego cambios en panel_de control

--- panel_de_control.cpp
+++ panel_de_control.cpp
@@ -9,25 +9,35 @@
 
 using namespace std;
 
-typedef int tpanel[4][4];
+typedef struct tcasilla{
+    int command;
+    int cdr;
+};
 
-double calculate(double x1, double y1, double x2, double y2) {
+typedef tcasilla tpanel[4][4];
+
+double calculate(double x1, double y1, double x2, double y2){
     return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
 }
 
-int main() {
-  int N;
-  cin>>N;
-  int commands[N];
-  tpanel panel = {
-                1,2,3,4,
-                5,6,7,8,
-                9,10,11,12,
-                13,14,15,16
- };
-  for(int i=0;i<N;i++)
-    cin>>commands[i];
+int main(){
+    int N;
+    cin>>N;
+    int commands[N];
+    tpanel panel =
+    {
+        {{1, 1},  {2, 1},  {3, 2},  {4, 2}},
+        {{5, 1},  {6, 1},  {7, 2},  {8, 2}},
+        {{9, 3},  {10, 3}, {11, 4}, {12, 4}},
+        {{13, 3}, {14, 3}, {15, 4}, {16, 4}}
+    };
 
+    for(int i=0; i<N; i++)
+        cin>>commands[i];
+    //for(int i=0; i<4; i++){
+       double d1 = calculate(panel[i])
+        ==
+    //}
 
-  return 0;
+    return 0;
 }
