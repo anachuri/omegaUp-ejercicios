3eaf50d1f257343a9e739227a603968aef4dce50
= <maxigroxx@gmail.com>
2026-08-19 21:29:10 -0300

    agrego casting.cpp

---
+++ casting.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct tparticipant{
+  double rating;
+  double probability;
+};
+
+int main(){
+    int n,k,       
+    cin>>n>>k;
+    tparticipant participans[n];
+    
+    for(int i=0;i<n;i++){
+      cin>>paticipants[i].rating;
+    }
+    
+    for(int i=0;i<n;i++){
+      cin>>paticipants[i].probability;
+    }
+  return 0;
+}
